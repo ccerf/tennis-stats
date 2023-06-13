ac00c150b3edaa820111ed997c86ae15b170f217
Carl <carl.cerf@gmail.com>
2023-06-13 02:34:41 +0200

    firebase init

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+	production: true,
+	firebase: {
+		apiKey: "AIzaSyC5uW1Rvm3d5uxXMtaWIYOtFqOPFlsh7aE",
+		authDomain: "atelier-tennis-stats.firebaseapp.com",
+		projectId: "atelier-tennis-stats",
+		storageBucket: "atelier-tennis-stats.appspot.com",
+		messagingSenderId: "253806204222",
+		appId: "1:253806204222:web:4636a1d5e9d80dcf4cc86f",
+	},
+};
